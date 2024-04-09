56a5e6ca6cdbc6079a4d00d9498dbe34f5b0c59b
vkpatva <vkpatva.it@gmail.com>
2024-04-09 08:10:16 +0530

    feat: rating api added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # RateITT
+
+Ratings on Chain, a blockchain app beyond NFTs and tokens, transcends ONDC's reputation system limits. Harnessing blockchain's power, it revolutionizes reputation management with transparency, immutability, and trust. Utilizing smart contracts on a decentralized ledger, it ensures seamless interoperability, boosting credibility of seller ratings. With Ratings on Chain, users navigate the digital marketplace confidently, empowered by a Solana Blockchain, setting new standards for transparency.
+
+About Project
+<div style="position: relative; padding-bottom: 63.49206349206349%; height: 0;"><iframe src="https://www.loom.com/share/88985d1992184e19a1ff5c6901fcd2e8" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,36 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/rating', (req: Request, res: Response) => {
+  try {
+    const { context, message } = req.body;
+    const { domain, country, city, action, core_version, transaction_id } = context;
+    const { ratings } = message;
+
+    console.log('Received rating data:');
+    console.log('Domain:', domain);
+    console.log('Country:', country);
+    console.log('City:', city);
+    console.log('Action:', action);
+    console.log('Core Version:', core_version);
+    console.log('Transaction ID:', transaction_id);
+
+    // todo : integerate rating with solana smart contract
+    console.log('Ratings:', ratings);
+
+    res.status(200).json({ success: true, message: 'Rating data received successfully.' });
+  } catch (error) {
+    console.error('Error processing rating request:', error);
+    res.status(500).json({ success: false, message: 'An error occurred while processing the rating request.' });
+  }
 });
 
 app.listen(port, () => {
